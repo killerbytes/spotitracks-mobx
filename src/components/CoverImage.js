fbaa560160d5fa388b3b4223bcc5f05dbd2499ab
Joel Carlos <joelcarlos02@gmail.com>
2023-09-21 18:00:39 +0800

    color extract

--- src/components/CoverImage.js
+++ src/components/CoverImage.js
@@ -1,8 +1,8 @@
-import { ColorExtractor } from 'react-color-extractor';
 import { observer } from 'mobx-react';
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import styled from 'styled-components';
 import { useStore } from 'stores';
+import Vibrant from 'node-vibrant';
 
 const CoverBG = styled.div`
   width: 60px;
@@ -18,15 +18,35 @@ const CoverBG = styled.div`
 const CoverImage = ({ img }) => {
   const { commonStore } = useStore();
 
-  const getColors = (colors) => {
-    commonStore.colors = colors;
+  const getColorsFromSwatches = (swatches) => {
+    const colors = [];
+
+    for (let swatch in swatches) {
+      if (swatches.hasOwnProperty(swatch) && swatches[swatch]) {
+        colors.push(swatches[swatch].getHex());
+      }
+    }
+
+    return colors;
   };
+
+  const getImage = useCallback(async () => {
+    if (img) {
+      Vibrant.from(img)
+        .getSwatches()
+        .then((s) => {
+          commonStore.setColors(getColorsFromSwatches(s));
+        });
+    }
+  }, [commonStore, img]);
+
+  useEffect(() => {
+    getImage();
+  }, [getImage]);
 
   return (
     <CoverBG $img={img}>
-      <ColorExtractor getColors={getColors}>
-        <img alt="cover" src={img} />
-      </ColorExtractor>
+      <img alt="cover" src={img} />
     </CoverBG>
   );
 };
